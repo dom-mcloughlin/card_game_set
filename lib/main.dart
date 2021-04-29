1cb5f6024ab75db9ae983b353af57e7f5eb07ee2
Dominic McLoughlin <dominic.mcloughlin@physics.ox.ac.uk>
2021-04-29 14:35:31 +0100

    Show card colour, tap to seleet card, show card number.

--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var names = <List>[
-    [1, 2, 3, 4],
-    [1, 2, 3, 3],
-    [1, 2, 2, 1]
-  ];
-  final indices = <int>[1, 2, 3, 4];
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final indices = <int>[1, 2, 3];
 
   @override
   Widget build(BuildContext context) {
@@ -58,12 +46,9 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title!),
       ),
       body: Center(
-        child: Column(children: [...buildCardRows()]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[...buildCardRows()]),
       ),
     );
   }
@@ -77,10 +62,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Create all combinations (3**4) of the four indices.
         // For example, a card combination of [1, 2, 1, 3] would correspond to
         // Shape 1, Colour 2, Texture 1, Number 3.
-        for (var d in this.indices)
-          for (var c in this.indices)
-            for (var b in this.indices)
-              for (var a in this.indices) [a, b, c, d]
+        for (int d in this.indices)
+          for (int c in this.indices)
+            for (int b in this.indices)
+              for (int a in this.indices) [a, b, c, d]
       ];
 
   List<PlayingCard> buildCards() =>
@@ -101,19 +86,87 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class PlayingCard extends StatelessWidget {
+class PlayingCard extends StatefulWidget {
   final List name;
 
   PlayingCard(this.name);
 
   @override
+  _PlayingCardState createState() => _PlayingCardState();
+}
+
+class _PlayingCardState extends State<PlayingCard> {
+  bool selectedCard = false;
+
+  final Color pinkColor = Color(0xffffa8cb);
+
+  final Color purpleColor = Color(0xff784283);
+
+  final Color redColor = Color(0xffe5000c);
+
+  final Color blackColor = Color(0xff000000);
+
+  final Color goldColor = Color(0xffffbf00);
+
+  int myShapeIndex() {
+    return widget.name.elementAt(4) - 1;
+  }
+
+  int myColorIndex() {
+    return widget.name.elementAt(1) - 1;
+  }
+
+  int myTextureIndex() {
+    return widget.name.elementAt(2) - 1;
+  }
+
+  int myNumberIndex() {
+    return widget.name.elementAt(3);
+  }
+
+  Color myColor() {
+    if (myColorIndex() == 0) {
+      return pinkColor;
+    }
+    if (myColorIndex() == 1) {
+      return purpleColor;
+    }
+    if (myColorIndex() == 2) {
+      return redColor;
+    }
+    return blackColor;
+  }
+
+  void toggleSelected() {
+    setState(() {
+      this.selectedCard = !this.selectedCard;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Card(
-        child: Center(
+        shape: selectedCard
+            ? new RoundedRectangleBorder(
+                side: new BorderSide(color: goldColor, width: 5.0),
+                borderRadius: BorderRadius.circular(4.0))
+            : new RoundedRectangleBorder(
+                side: new BorderSide(color: Colors.white, width: 0),
+                borderRadius: BorderRadius.circular(4.0)),
+        color: myColor(),
+        child: new InkWell(
+          onTap: () {
+            this.toggleSelected();
+          },
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(this.name.map((i) => i.toString()).join(",")),
+              for (int ii = 0; ii < this.myNumberIndex(); ii++)
+                Text(
+                  this.widget.name.map((i) => i.toString()).join(","),
+                  textAlign: TextAlign.center,
+                ),
             ],
           ),
         ),
